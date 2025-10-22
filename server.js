58e45cab46ab244461f0bb03930dad74001d4fd4
sifat-samin <sifatsamin19@gmail.com>
2025-10-22 16:18:37 +0600

    Route Tested

--- server.js
+++ server.js
@@ -8,6 +8,26 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/api/ideas', (req, res) => {
+    const ideas = [
+        { id: 1, title: "title_1", description: 'This is Idea 1' },
+        { id: 2, title: "title_2", description: 'This is Idea 2' },
+        { id: 3, title: "title_3", description: 'This is Idea 3' },
+        { id: 4, title: "title_4", description: 'This is Idea 4' },
+    ];
+
+    res.json(ideas)
+});
+
+app.post('/api/ideas', (req, res) => {
+    const { title, description } = req.body;
+
+    console.log(description)
+    res.send(`Data Processed Title: ${title}`)
+})
 
 app.listen(PORT, () => {
     console.log("Server is Running on PORT: ", PORT);
